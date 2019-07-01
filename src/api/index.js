9720c7ac430b364a7425b2e60f878251e6447537
xn213 <812725903@qq.com>
2019-07-02 01:16:13 +0800

    :sunny::smirk::muscle::metal:add api comments done

--- src/api/index.js
+++ src/api/index.js
@@ -17,6 +17,10 @@ export function getMM(page){
   })
 }
 
+/**
+ * 点击home页图片显示大图详情, 获取图片详情数据
+ * @param {Object} timeObj 时间对象: {Y:'2019', M: '02', D: '13' }
+ */
 export function getDetail(timeObj){
   return Axios({
     method: 'get',
@@ -24,9 +28,14 @@ export function getDetail(timeObj){
   })
 }
 
+/**
+ * 获取列表数据
+ * @param { string } type 类型: 前端/iOS/Android
+ * @param { Number } page 页码
+ */
 export function getListData(type, page){
-return Axios({
-  method: 'get',
-  url: `http://gank.io/api/data/${type}/10/${page}`
-})
+  return Axios({
+    method: 'get',
+    url: `http://gank.io/api/data/${type}/10/${page}`
+  })
 }
